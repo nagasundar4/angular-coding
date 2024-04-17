73406d400b54f5d285e8340743e55f3975c54ec3
Nagarajan Sundaram <nagasundar@gmail.com>
2024-04-17 16:18:09 -0400

    Add user functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { UserComponent } from './user/user.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, UserComponent],
+  template: `
+    <div>
+      <app-user />
+    </div>
+  `,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-coding';
